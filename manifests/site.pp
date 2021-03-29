49c477cb652415e95412d8517c45f24651d3d209
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2021-03-29 20:07:20 +0000

    Add yumrepo and packages

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,33 @@
 node default {
-
+  package{'vim':}
 }
 
+node 'mds1' {
+  package{'vim':}
+
+  yumrepo { 'lustre-server':
+    baseurl => 'https://downloads.whamcloud.com/public/lustre/latest-2.12-release/el7/server/',
+    gpgcheck => 0
+  }
+  yumrepo { 'e2fsprogs':
+    baseurl => 'https://downloads.whamcloud.com/public/e2fsprogs/latest/el7/',
+    gpgcheck => 0
+  }
+
+  package { ['lustre', 'kmod-lustre-osd-ldiskfs']: }
+}
+
+node /oss\d+/ {
+  package{'vim':}
+
+  yumrepo { 'lustre-server':
+    baseurl => 'https://downloads.whamcloud.com/public/lustre/latest-2.12-release/el7/server/',
+    gpgcheck => 0
+  }
+  yumrepo { 'e2fsprogs':
+    baseurl => 'https://downloads.whamcloud.com/public/e2fsprogs/latest/el7/',
+    gpgcheck => 0
+  }
+
+  package { ['lustre', 'kmod-lustre-osd-ldiskfs']: }
+}
