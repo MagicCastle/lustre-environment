0f6a0fe898ad9507c8c34874f9212f6edd9d3ce2
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2021-03-30 01:34:50 +0000

    Populate /etc/hosts with terraform instances

--- manifests/site.pp
+++ manifests/site.pp
@@ -7,6 +7,20 @@ node default {
     gpgcheck => 0
   }
   package { ['kmod-lustre-client', 'lustre-client']: }
+
+  $instances = lookup('terraform.instances')
+$host_template = @(END)
+127.0.0.1 localhost localhost.localdomain localhost4 localhost4.localdomain4
+<% @instances.each do |key, values| -%>
+<%= values['local_ip'] %> <%= key %> <% if values['tags'].include?('puppet') %>puppet<% end %>
+<% end -%>
+END
+
+  file { '/etc/hosts':
+    ensure  => file,
+    content => inline_template($host_template)
+  }
+
 }
 
 node 'mds1' {
